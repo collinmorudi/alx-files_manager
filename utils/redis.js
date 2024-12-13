48321e381f16afa8eae2dc208b21366798db6251
mmorudi023 <mmorudi023@student.wethinkcode.co.za>
2024-12-13 20:55:01 +0200

    Modified redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -1,11 +1,9 @@
-// utils/redis.js
-// eslint-disable-next-line import/no-unresolved
-import { createClient } from 'redis';
+const redis = require('redis');
 
 class RedisClient {
   constructor() {
-    this.client = createClient();
-    this.client.on('error', (err) => console.error(`Redis client error: ${err}`));
+    this.client = redis.createClient();
+    this.client.on('error', (err) => console.log(`Redis Client Error: ${err}`));
   }
 
   isAlive() {
@@ -15,26 +13,35 @@ class RedisClient {
   async get(key) {
     return new Promise((resolve, reject) => {
       this.client.get(key, (err, value) => {
-        if (err) reject(err);
-        resolve(value);
+        if (err) {
+          reject(err);
+        } else {
+          resolve(value);
+        }
       });
     });
   }
 
   async set(key, value, duration) {
     return new Promise((resolve, reject) => {
-      this.client.setex(key, duration, value, (err) => {
-        if (err) reject(err);
-        resolve(true);
+      this.client.set(key, value, 'EX', duration, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
       });
     });
   }
 
   async del(key) {
     return new Promise((resolve, reject) => {
-      this.client.del(key, (err) => {
-        if (err) reject(err);
-        resolve(true);
+      this.client.del(key, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
       });
     });
   }
